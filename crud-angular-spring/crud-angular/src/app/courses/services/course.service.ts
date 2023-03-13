05d804092e598885f967cf3ec665ec5a2d06f8c8
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-03-13 15:51:32 -0300

    sucess

--- crud-angular-spring/crud-angular/src/app/courses/services/course.service.ts
+++ crud-angular-spring/crud-angular/src/app/courses/services/course.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 
 import { Course } from '../model/course';
 
@@ -12,6 +13,9 @@ export class CourseService {
   constructor(private httpClient: HttpClient) { }
 
   list(){
-    return this.httpClient.get<Course[]>(this.API);
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
