ccd90b3baca08f17caa6bbe64e58ed907989d278
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-03-16 16:03:58 -0300

    sucess

--- crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
+
+  courses: any[] = [];
+
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
