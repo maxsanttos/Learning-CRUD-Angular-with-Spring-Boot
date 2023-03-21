016bf7974eb553ca0baf84fc0d8062ef4bad0920
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-03-21 19:19:58 -0300

    tratamento de erros

--- crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 @NgModule({
   exports: [
     MatTableModule,
     MatCardModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule
   ],
+
 
 })
 export class AppMaterialModule { }
