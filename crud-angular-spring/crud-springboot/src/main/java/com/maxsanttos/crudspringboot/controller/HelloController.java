4060c0572322bfe868c323814cf91b4002deb7d7
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-05-09 15:26:25 -0300

    up

---
+++ crud-angular-spring/crud-springboot/src/main/java/com/maxsanttos/crudspringboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.maxsanttos.crudspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    
+    @GetMapping
+    public String hello(){
+        return "Hello, world. Max!";
+    }
+}
