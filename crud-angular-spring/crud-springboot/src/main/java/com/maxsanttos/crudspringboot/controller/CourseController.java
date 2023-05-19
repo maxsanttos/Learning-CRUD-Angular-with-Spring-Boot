0a772fb21ac6769eb7e7687304f31374ae086bda
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-05-19 15:10:39 -0300

    up

--- crud-angular-spring/crud-springboot/src/main/java/com/maxsanttos/crudspringboot/controller/CourseController.java
+++ crud-angular-spring/crud-springboot/src/main/java/com/maxsanttos/crudspringboot/controller/CourseController.java
@@ -1,15 +1,18 @@
 package com.maxsanttos.crudspringboot.controller;
 
+import org.hibernate.mapping.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
-public class HelloController {
-    
+@RequestMapping("/api/courses")
+public class CourseController {
+
     @GetMapping
-    public String hello(){
-        return "Hello, world. Max!";
+    //@RequestMapping(method = RequestMethod.GET)
+    public List<Object> list(){
+        return null;
     }
 }
