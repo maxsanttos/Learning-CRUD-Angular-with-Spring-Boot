74fa6cc8d25d7c85bc8c6703b48bc0c4680878fb
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-05-24 21:09:43 -0300

    UP

--- crud-angular-spring/crud-springboot/src/main/java/com/maxsanttos/crudspringboot/controller/CourseController.java
+++ crud-angular-spring/crud-springboot/src/main/java/com/maxsanttos/crudspringboot/controller/CourseController.java
@@ -2,14 +2,11 @@ package com.maxsanttos.crudspringboot.controller;
 
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.maxsanttos.crudspringboot.model.Course;
 import com.maxsanttos.crudspringboot.repository.CourseRepository;
-
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -17,9 +14,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-
     private final CourseRepository courseRepository;
-
+    
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
